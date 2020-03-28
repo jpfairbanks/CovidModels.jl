424a5b9c593b175485c237285104c1b095dfe740
James Fairbanks <james.fairbanks@gtri.gatech.edu>
2020-03-28 14:05:50 -0400

    document states and parameters of validation tests

--- test/validation.jl
+++ test/validation.jl
@@ -5,17 +5,11 @@ using OrdinaryDiffEq
 import OrdinaryDiffEq: ODEProblem
 using Petri
 using Test
-# using SemanticModels.ModelTools.CategoryTheory
-# import SemanticModels.ModelTools.CategoryTheory: undecorate, ⊔
 using SemanticModels.ModelTools.PetriModels
 
 
 
-# X = FinSet(1)
-# Fseir = compose(exposure(X,X,X),otimes(spontaneous(X,X),id(X)),mmerge(X),mcopy(X),otimes(id(X),spontaneous(X,X)))
-# states are [S, I, E, R]
-# m_seir = left(Fseir.f).d[1]
-
+# states are [S, I, R]
 Psir = PetriModel(
           Petri.Model(1:3,[
             ([1,2],[2,2]), # exposure
@@ -28,6 +22,7 @@ u0 = zeros(Float64, length(m.S))
 u0[1]  = 10000
 u0[2]  = 1
 
+# probability of infection, recovery
 β = [10/sum(u0), 1/5]
 
 tspan = (0,100.0)
@@ -36,19 +31,21 @@ sol = OrdinaryDiffEq.solve(prob, alg=Tsit5())
 
 savedata(sol, "S,I,R", "sirdata.csv")
 
+# states are [S, I, R, D]
 Psird = PetriModel(
-          Petri.Model(1:3,[
+          Petri.Model(1:4,[
             ([1,2],[2,2]), # exposure
             ([2],[3]),     # recovery
             ([2],[4]),     # death
             ], missing, missing))
 
-m = Psir.model
+m = Psird.model
 
 u0 = zeros(Float64, length(m.S))
 u0[1]  = 10000
 u0[2]  = 1
 
+# probability of infection, recovery, death
 β = [10/sum(u0), 1/5, 1/10]
 
 tspan = (0,100.0)
@@ -57,6 +54,7 @@ sol = OrdinaryDiffEq.solve(prob, alg=Tsit5())
 
 savedata(sol, "S,I,R,D", "sirddata.csv")
 
+# states are [S, I, E, R]
 Pseir = PetriModel(
           Petri.Model(1:5,[
             ([1,2],[3,2]), # exposure
@@ -70,6 +68,7 @@ u0 = zeros(Float64, length(m.S))
 u0[1]  = 10000
 u0[2]  = 1
 
+# probability of exposure, progression, recovery
 β = [10/sum(u0), 1/2, 1/5]
 
 tspan = (0,100.0)
@@ -77,6 +76,9 @@ prob = ODEProblem(fluxes(m), u0, tspan, β)
 sol = OrdinaryDiffEq.solve(prob, alg=Tsit5())
 
 savedata(sol, "S,I,E,R", "seirdata.csv")
+
+
+# states are [S, I, E, R, D]
 Pseird = PetriModel(
           Petri.Model(1:5,[
             ([1,2],[3,2]), # exposure
@@ -92,6 +94,7 @@ u0[1]  = 10000
 u0[2]  = 1
 
 seirdparams() = begin
+    # probability of exposure, progression, recovery, death
     βseird = [10/sum(u0), 1/2, 1/5, 1/16]
 end
 
