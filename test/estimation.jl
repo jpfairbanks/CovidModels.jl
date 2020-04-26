749780955c60a02a35647b22159ea9cffe3f49f5
James Fairbanks <james.fairbanks@gtri.gatech.edu>
2020-04-26 12:23:58 -0400

    add code to fit SEIIRD model to data.

--- test/estimation.jl
+++ test/estimation.jl
@@ -6,6 +6,20 @@ import OrdinaryDiffEq: ODEProblem
 using Petri
 using Test
 using Plots
+using StatsPlots
+using Colors
+
+function fitplot(sol, df)
+    p = plot(sol, vars=[2, 4, 5, 6], label=["IA" "I" "R" "D"])
+    @df df scatter!(p,
+            0:length(italy.Deaths)-1,
+            [:Cases, :Deaths, :Recoveries],
+            label=["cases" "deaths" "recoveries"],
+            marker=true,
+            color=[RGB(1,0,0) RGB(0.5,0,0.5) RGB(0,1,0)]
+            )
+    return p
+end
 
 function makeplots_seir(sol, prefix)
     mkpath(dirname(prefix))
@@ -136,3 +150,103 @@ italy = @from i in df begin
 end
 
 caseplot(italy)
+
+function lossfunc(df::DataFrame, m, u0)
+    tspan = (0.0, length(df.Deaths)-1)
+    mean(x) = sum(x)/length(x)
+    t = tspan[1]:tspan[end]
+    f(sol) = begin
+        deaths = [u[end] for u in sol.(t)]
+        recovr = [u[end-1] for u in sol.(t)]
+        infsym = [u[end-2] for u in sol.(t)]
+        dt = mean((deaths .- df.Deaths).^2) / mean(df.Deaths)
+        rt = mean((recovr .- df.Recoveries).^2) / mean(df.Recoveries)
+        it = mean((infsym .- df.Cases).^2) / mean(df.Cases)
+        return dt + rt + it
+    end
+
+    g(θ) = begin
+        prob = ODEProblem(fluxes(m), u0, tspan, θ)
+        sol = OrdinaryDiffEq.solve(prob,Tsit5(),reltol=1e-8,abstol=1e-8)
+        neg = sum(abs.(θ[θ .< 0]))
+        λ = 10000000
+        # if final deaths > final recoveries then penalty
+        # deathpenalty = sol.u[end][end] > sol.u[end][end-1] ? 1 : 0
+        # iapenalty = sum(tanh.([u[3] - u[2] for u in sol.(t)]).+1)
+        iapenalty = 0
+        lossval, regval = f(sol), λ*(neg + iapenalty)
+        # lossval, regval = f(sol) , λ*(neg + deathpenalty)
+        println("loss:$lossval\tregval:$regval")
+        lossval+regval
+    end
+    return g
+end
+
+u0_italy = [60000000.0, 0, 1, 0, 0, 0]
+ℓ = lossfunc(italy, m, u0_italy)
+
+ℓ(β)
+
+using Optim
+
+function calibrate(k)
+    β = [40/5sum(u0), 11/2, 1/1, 2/13, 1/18, 1/35]/k
+    prob = ODEProblem(fluxes(m), u0_italy, (0.0,95), β)
+    sol = OrdinaryDiffEq.solve(prob,Tsit5(),reltol=1e-8,abstol=1e-8)
+    # plot(sol, vars=[2, 4, 5, 6, 3], label=["IA" "I" "R" "D" "E"])
+    # p = fitplot(sol, italy)
+    p = plot(sol, vars=[4, 5, 6], label=["I" "R" "D"],
+            color=[RGB(1,0,0) RGB(0,1,0) RGB(0.5,0,0.5)])
+    @df df scatter!(p,
+            0:length(italy.Deaths)-1,
+            [:Cases, :Deaths, :Recoveries],
+            label=["cases" "deaths" "recoveries"],
+            marker=true,
+            color=[RGB(1,0,0) RGB(0.5,0,0.5) RGB(0,1,0)]
+            )
+    return β, sol, p
+end
+
+calibrate(15.5)[end]
+
+β = [40/5sum(u0), 11/2, 1/1, 2/13, 1/18, 1/35]/15
+# β = [60/5sum(u0), 11/2, 1/1, 2/13, 1/24, 1/39]/2
+prob = ODEProblem(fluxes(m), u0_italy, (0.0,64), β)
+sol = OrdinaryDiffEq.solve(prob,Tsit5(),reltol=1e-8,abstol=1e-8)
+plot(sol, vars=[2, 4, 5, 6, 3], label=["IA" "I" "R" "D" "E"])
+fitplot(sol, italy)
+
+lb, ub = zeros(Float64, 6), 100*ones(Float64, 6)
+
+res = optimize(ℓ, β)
+# res = optimize(ℓ, lb, ub, β, Fminbox(NelderMead()))
+@show res.minimizer
+@show res.minimizer -β
+
+# candidate minimizer
+β̂ = [
+ 9.478916256341902e-9
+ 0.2915773795849941
+ 0.09434402982528367
+ 0.0061044262074341285
+ 0.016241417062038518
+ 0.02234216514573622
+]
+
+prob = ODEProblem(fluxes(m), u0_italy, (0.0,128), res.minimizer)
+sol = OrdinaryDiffEq.solve(prob,Tsit5(),reltol=1e-8,abstol=1e-8)
+fitplot(sol, italy)
+
+prob = ODEProblem(fluxes(m), u0_italy, (0.0,70), res.minimizer)
+sol = OrdinaryDiffEq.solve(prob,Tsit5(),reltol=1e-8,abstol=1e-8)
+fitplot(sol, df)
+
+# trying to add constraints to the parameter space
+realparams(θ) = [θ[1:4]..., θ[5], θ[5]/5]
+res = optimize(θ->ℓ(realparams(θ)), β[1:6])
+@show res.minimizer
+@show res.minimizer -β
+
+prob = ODEProblem(fluxes(m), u0_italy, (0.0,128), realparams(res.minimizer))
+sol = OrdinaryDiffEq.solve(prob,Tsit5(),reltol=1e-8,abstol=1e-8)
+fitplot(sol, df)
